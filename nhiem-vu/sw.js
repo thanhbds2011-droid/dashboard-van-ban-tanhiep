eb491ac315150411254ea0ce5b6792b8c763fca5
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-14 00:40:48 +0700

    Update sw.js

--- nhiem-vu/sw.js
+++ nhiem-vu/sw.js
@@ -1,1 +1,95 @@
+const CACHE_NAME = "nhiem-vu-shell-v20260714.83";
 
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./firebase-config.js",
+  "./manifest.webmanifest",
+  "./offline.html",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/apple-touch-icon.png",
+  "./icons/favicon-64.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data?.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener("fetch", (event) => {
+  const request = event.request;
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  const url = new URL(request.url);
+
+  // Không can thiệp các yêu cầu Firebase, Google hoặc CDN ngoài GitHub Pages.
+  if (url.origin !== self.location.origin) {
+    return;
+  }
+
+  if (request.mode === "navigate") {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then((cache) => cache.put("./index.html", copy))
+            .catch(() => {});
+
+          return response;
+        })
+        .catch(() =>
+          caches.match("./offline.html")
+        )
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        if (response.ok) {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then((cache) => cache.put(request, copy))
+            .catch(() => {});
+        }
+
+        return response;
+      })
+      .catch(() =>
+        caches.match(request)
+      )
+  );
+});
