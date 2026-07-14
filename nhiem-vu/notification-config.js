d3dbd2ebf4ec9b5064aeabf466c7f75d9cc6e565
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-14 14:53:11 +0700

    Create notification-config.js

---
+++ nhiem-vu/notification-config.js
@@ -0,0 +1,9 @@
+/*
+ * Bước hiện tại chưa triển khai Web App gửi tự động.
+ * Giữ giá trị này để app.js nạp bình thường.
+ * Sau khi triển khai Apps Script Web App,
+ * thay bằng URL kết thúc /exec.
+ */
+
+export const NOTIFICATION_WEB_APP_URL =
+  "DAN_LINK_WEB_APP_APPS_SCRIPT_VAO_DAY";
