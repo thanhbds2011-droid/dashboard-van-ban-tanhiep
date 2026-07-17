dede08c22db589a7e3404ffd3dbfd32a4d4b7f24
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-17 23:33:23 +0700

    Update pwa.js

--- nhiem-vu/pwa.js
+++ nhiem-vu/pwa.js
@@ -4,7 +4,7 @@
  * =========================================================
  */
 
-const PWA_VERSION = "20260717.2100";
+const PWA_VERSION = "20260717.2200";
 const UPDATE_CHECK_INTERVAL = 30 * 60 * 1000;
 
 let deferredInstallPrompt = null;
