47b309bdd1bcd931bc0ed4540463aaa8f6238784
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-17 13:33:57 +0700

    Update sw.js

--- nhiem-vu/sw.js
+++ nhiem-vu/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "20260717.1200";
+const APP_VERSION = "20260717.1300";
 const CACHE_NAME = `nhiem-vu-shell-v${APP_VERSION}`;
 const APP_BASE_URL = new URL("./", self.location.href);
 
