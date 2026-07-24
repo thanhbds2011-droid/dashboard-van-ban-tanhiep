d485b5ba7e9b484cf99cbe7e2103fdfc5aba80b1
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-25 00:02:07 +0700

    Update plans-view.js

--- nhiem-vu/modules/plans/plans-view.js
+++ nhiem-vu/modules/plans/plans-view.js
@@ -1,2 +1,2 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js";
+import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260724.FINAL2";
 export async function renderPlansView(outlet){ await renderKpiWorkflow(outlet); }
