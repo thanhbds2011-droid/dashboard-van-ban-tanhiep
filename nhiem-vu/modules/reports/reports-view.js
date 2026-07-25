56fd0fecdb284baa13513fb15000fe28aaf840e2
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-25 13:43:01 +0700

    Update reports-view.js

--- nhiem-vu/modules/reports/reports-view.js
+++ nhiem-vu/modules/reports/reports-view.js
@@ -1,2 +1,4 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260724.FINAL2";
-export async function renderReportsView(outlet){ await renderKpiWorkflow(outlet,{openReport:true}); }
+import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260725.COMPLETE1";
+export async function renderReportsView(outlet) {
+  await renderKpiWorkflow(outlet, { mode: "reports", openReport: true });
+}
