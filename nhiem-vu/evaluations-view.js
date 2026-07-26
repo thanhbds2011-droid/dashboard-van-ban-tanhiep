04380cad3236da86432bb3b702c6266a6a4b10e4
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-26 10:54:23 +0700

    Delete nhiem-vu/evaluations-view.js

--- nhiem-vu/evaluations-view.js
+++
@@ -1,2 +0,0 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260725.FINALV1";
-export async function renderEvaluationsView(outlet) { await renderKpiWorkflow(outlet, { mode: "plans" }); }
