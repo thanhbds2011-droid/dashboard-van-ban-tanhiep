b22aa2b3331a7b0d88f167b86a6afc6fa7bc0f71
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-26 10:54:46 +0700

    Delete nhiem-vu/reports-view.js

--- nhiem-vu/reports-view.js
+++
@@ -1,4 +0,0 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260725.FINALV1";
-export async function renderReportsView(outlet) {
-  await renderKpiWorkflow(outlet, { mode: "reports", openReport: true });
-}
