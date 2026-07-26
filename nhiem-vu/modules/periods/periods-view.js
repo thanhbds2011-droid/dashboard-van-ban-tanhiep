7e8be00bed41724265c5bf865b4dbdb42af41a8c
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-26 10:38:23 +0700

    Update periods-view.js

--- nhiem-vu/modules/periods/periods-view.js
+++ nhiem-vu/modules/periods/periods-view.js
@@ -1,2 +1,2 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260724.FINAL2";
+import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260724.FIX_SCOPE1";
 export async function renderPeriodsView(outlet){ await renderKpiWorkflow(outlet); }
