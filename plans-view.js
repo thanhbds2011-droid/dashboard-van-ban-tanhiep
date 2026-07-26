717d310c68297abb06f4e513cfabaa6e34e0eb55
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-26 16:20:11 +0700

    Delete plans-view.js

--- plans-view.js
+++
@@ -1,4 +0,0 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260726.PRODUCTION";
-export async function renderPlansView(outlet) {
-  await renderKpiWorkflow(outlet, { mode: "plans" });
-}
