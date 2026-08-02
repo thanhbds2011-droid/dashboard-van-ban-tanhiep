423246f5b79dde2248b3b83d3123c29ed4ffa022
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-02 11:41:14 +0700

    Add files via upload

--- nhiem-vu/modules/standard-tasks/standard-tasks-view.js
+++ nhiem-vu/modules/standard-tasks/standard-tasks-view.js
@@ -1,10 +1,10 @@
 import { UserContext } from "../../core/user-context.js";
-import { Permissions } from "../../core/permissions.js?v=20260801.V1_3_1";
+import { Permissions } from "../../core/permissions.js?v=20260801.V1_5_0";
 import { ToastService } from "../../core/toast-service.js";
-import { StandardTaskReadService } from "../../services/standard-task-read-service.js?v=20260801.V1_3_1";
-import { PeriodReadService } from "../../services/period-read-service.js?v=20260801.V1_3_0";
-import { StandardTaskWriteService } from "../../services/standard-task-write-service.js?v=20260801.V1_3_0";
-import { TaskRegistrationService } from "../../services/task-registration-service.js?v=20260801.V1_3_0";
+import { StandardTaskReadService } from "../../services/standard-task-read-service.js?v=20260801.V1_5_0";
+import { PeriodReadService } from "../../services/period-read-service.js?v=20260801.V1_5_0";
+import { StandardTaskWriteService } from "../../services/standard-task-write-service.js?v=20260801.V1_5_0";
+import { TaskRegistrationService } from "../../services/task-registration-service.js?v=20260801.V1_5_0";
 
 let currentCatalogAccess = { canManage: false, manageableDepartmentIds: [] };
 
