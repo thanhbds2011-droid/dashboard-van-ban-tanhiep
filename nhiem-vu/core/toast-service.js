df62000807f900984d62515b919b94dba980a290
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-04 22:09:12 +0700

    Add files via upload

--- nhiem-vu/core/toast-service.js
+++ nhiem-vu/core/toast-service.js
@@ -1,4 +1,4 @@
-import { friendlyErrorMessage } from "./friendly-error.js?v=20260804.V1_8_1";
+import { friendlyErrorMessage } from "./friendly-error.js?v=20260804.V1_8_2";
 
 /** Production 3B.2 - Toast Service */
 const CONTAINER_ID = "v3ToastContainer";
