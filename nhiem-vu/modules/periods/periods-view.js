a540aabe993b0f8b23c38dca92cc63cdf7fef2e2
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-07 16:49:39 +0700

    Add files via upload

--- nhiem-vu/modules/periods/periods-view.js
+++ nhiem-vu/modules/periods/periods-view.js
@@ -1,2 +1,2 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260805.V1_9_3";
+import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260806.V1_9_4";
 export async function renderPeriodsView(outlet){ await renderKpiWorkflow(outlet); }
