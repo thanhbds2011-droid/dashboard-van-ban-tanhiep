baf9d23a5367e63cf918ae06ec49eea6feaf12ef
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-08 14:16:07 +0700

    Update app-version.js

--- nhiem-vu/core/app-version.js
+++ nhiem-vu/core/app-version.js
@@ -2,3 +2,4 @@
 export const APP_VERSION = "1.10.0";
 export const BUILD_VERSION = "20260808.V1_10_0";
 export const CACHE_NAME = "nhiem-vu-20260808-v1-10-0";
+export const APP_VERSION_LABEL = `V${APP_VERSION}`;
