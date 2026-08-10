c13ce039036ffe29e2cb446b659ad584253546d9
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-10 20:57:10 +0700

    Add files via upload

--- nhiem-vu/core/app-version.js
+++ nhiem-vu/core/app-version.js
@@ -1,5 +1,5 @@
 /** Phiên bản phát hành tập trung. */
-export const APP_VERSION = "1.10.8";
-export const BUILD_VERSION = "20260810.V1_10_8";
-export const CACHE_NAME = "nhiem-vu-20260810-v1-10-8";
+export const APP_VERSION = "1.10.9";
+export const BUILD_VERSION = "20260810.V1_10_9";
+export const CACHE_NAME = "nhiem-vu-20260810-v1-10-9";
 export const APP_VERSION_LABEL = `V${APP_VERSION}`;
