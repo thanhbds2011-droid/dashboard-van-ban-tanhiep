e8104a51bd4b85ba80c712a821b14d44ae5e2fab
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-24 10:11:56 +0700

    Add files via upload

--- nhiem-vu/modules/standard-tasks/standard-tasks-view.js
+++ nhiem-vu/modules/standard-tasks/standard-tasks-view.js
@@ -1,11 +1,11 @@
-import { UserContext } from "../../core/user-context.js?v=20260824.V1_13_0";
-import { Permissions } from "../../core/permissions.js?v=20260824.V1_13_0";
-import { ToastService } from "../../core/toast-service.js?v=20260824.V1_13_0";
-import { StandardTaskReadService } from "../../services/standard-task-read-service.js?v=20260824.V1_13_0";
-import { PeriodReadService } from "../../services/period-read-service.js?v=20260824.V1_13_0";
-import { StandardTaskWriteService } from "../../services/standard-task-write-service.js?v=20260824.V1_13_0";
-import { TaskRegistrationService } from "../../services/task-registration-service.js?v=20260824.V1_13_0";
-import { deriveDeadlinePlan, deadlineRuleDescription, requiresManualDeadline } from "../../core/deadline-engine.js?v=20260824.V1_13_0";
+import { UserContext } from "../../core/user-context.js?v=20260824.V1_14_0";
+import { Permissions } from "../../core/permissions.js?v=20260824.V1_14_0";
+import { ToastService } from "../../core/toast-service.js?v=20260824.V1_14_0";
+import { StandardTaskReadService } from "../../services/standard-task-read-service.js?v=20260824.V1_14_0";
+import { PeriodReadService } from "../../services/period-read-service.js?v=20260824.V1_14_0";
+import { StandardTaskWriteService } from "../../services/standard-task-write-service.js?v=20260824.V1_14_0";
+import { TaskRegistrationService } from "../../services/task-registration-service.js?v=20260824.V1_14_0";
+import { deriveDeadlinePlan, deadlineRuleDescription, requiresManualDeadline } from "../../core/deadline-engine.js?v=20260824.V1_14_0";
 
 let currentCatalogAccess = {
   canManage: false,
