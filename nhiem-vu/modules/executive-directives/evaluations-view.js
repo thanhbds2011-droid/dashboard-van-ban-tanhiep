f162fc2b2b66fa438f572454cd309b6c2fc3ba85
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-24 14:08:36 +0700

    Add files via upload

---
+++ nhiem-vu/modules/executive-directives/evaluations-view.js
@@ -0,0 +1,4 @@
+import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260824.V1_15_0";
+export async function renderEvaluationsView(outlet) {
+  await renderKpiWorkflow(outlet, { mode: "evaluations" });
+}
