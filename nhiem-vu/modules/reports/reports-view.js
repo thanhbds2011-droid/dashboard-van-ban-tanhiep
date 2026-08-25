dbe2cd8b5c2f39214b121fe2b5fd99abe3b20831
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-25 16:03:30 +0700

    Add files via upload

--- nhiem-vu/modules/reports/reports-view.js
+++ nhiem-vu/modules/reports/reports-view.js
@@ -1,4 +1,4 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260825.V1_16_1";
+import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260825.V1_17_0";
 export async function renderReportsView(outlet) {
   await renderKpiWorkflow(outlet, { mode: "reports" });
 }
