46f2c147d519fc125cda8357f49de2eac6c4a795
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-26 11:24:58 +0700

    Add files via upload

--- nhiem-vu/modules/plans/plans-view.js
+++ nhiem-vu/modules/plans/plans-view.js
@@ -1,4 +1,4 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260826.V1_18_2";
+import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260826.V1_18_3";
 export async function renderPlansView(outlet) {
   await renderKpiWorkflow(outlet, { mode: "plans" });
 }
