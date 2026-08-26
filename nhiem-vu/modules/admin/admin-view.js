51f7a4953fd3c621404fc40c6c7214337d1ee09d
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-26 08:49:25 +0700

    Add files via upload

--- nhiem-vu/modules/admin/admin-view.js
+++ nhiem-vu/modules/admin/admin-view.js
@@ -1,9 +1,9 @@
-import { UserContext } from "../../core/user-context.js?v=20260825.V1_18_0";
-import { Permissions } from "../../core/permissions.js?v=20260825.V1_18_0";
-import { ToastService } from "../../core/toast-service.js?v=20260825.V1_18_0";
-import { ModalService } from "../../core/modal-service.js?v=20260825.V1_18_0";
-import { AdminReadService } from "../../services/admin-read-service.js?v=20260825.V1_18_0";
-import { AdminMaintenanceService } from "../../services/admin-maintenance-service.js?v=20260825.V1_18_0";
+import { UserContext } from "../../core/user-context.js?v=20260826.V1_18_1";
+import { Permissions } from "../../core/permissions.js?v=20260826.V1_18_1";
+import { ToastService } from "../../core/toast-service.js?v=20260826.V1_18_1";
+import { ModalService } from "../../core/modal-service.js?v=20260826.V1_18_1";
+import { AdminReadService } from "../../services/admin-read-service.js?v=20260826.V1_18_1";
+import { AdminMaintenanceService } from "../../services/admin-maintenance-service.js?v=20260826.V1_18_1";
 
 let currentDiagnostic = null;
 
