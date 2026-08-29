d05d12cb173878a09af782376cb28c40fd7c67f8
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-29 16:39:44 +0700

    Add files via upload

--- nhiem-vu/modules/dashboard/dashboard-view.js
+++ nhiem-vu/modules/dashboard/dashboard-view.js
@@ -1,8 +1,8 @@
-import { UserContext } from "../../core/user-context.js?v=20260826.V1_19_0";
-import { Permissions } from "../../core/permissions.js?v=20260826.V1_19_0";
-import { ToastService } from "../../core/toast-service.js?v=20260826.V1_19_0";
-import { DashboardReadService } from "../../services/dashboard-read-service.js?v=20260826.V1_19_0";
-import { TaskReadService } from "../../services/task-read-service.js?v=20260826.V1_19_0";
+import { UserContext } from "../../core/user-context.js?v=20260829.V1_20_0";
+import { Permissions } from "../../core/permissions.js?v=20260829.V1_20_0";
+import { ToastService } from "../../core/toast-service.js?v=20260829.V1_20_0";
+import { DashboardReadService } from "../../services/dashboard-read-service.js?v=20260829.V1_20_0";
+import { TaskReadService } from "../../services/task-read-service.js?v=20260829.V1_20_0";
 let currentData = null;
 let dashboardRenderSequence = 0;
 let dashboardDepartmentScope = "ALL";
