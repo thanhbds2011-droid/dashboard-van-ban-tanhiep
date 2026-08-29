367a32ac37d75b7b7bd74dfcc5ae75a96381636f
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-08-29 16:40:17 +0700

    Add files via upload

--- nhiem-vu/modules/executive-directives/executive-directives-view.js
+++ nhiem-vu/modules/executive-directives/executive-directives-view.js
@@ -1,10 +1,10 @@
-import { UserContext } from "../../core/user-context.js?v=20260826.V1_19_0";
-import { Permissions } from "../../core/permissions.js?v=20260826.V1_19_0";
-import { ToastService } from "../../core/toast-service.js?v=20260826.V1_19_0";
-import { ModalService } from "../../core/modal-service.js?v=20260826.V1_19_0";
-import { DepartmentReadService } from "../../services/department-read-service.js?v=20260826.V1_19_0";
-import { UserReadService } from "../../services/user-read-service.js?v=20260826.V1_19_0";
-import { ExecutiveDirectiveService } from "../../services/executive-directive-service.js?v=20260826.V1_19_0";
+import { UserContext } from "../../core/user-context.js?v=20260829.V1_20_0";
+import { Permissions } from "../../core/permissions.js?v=20260829.V1_20_0";
+import { ToastService } from "../../core/toast-service.js?v=20260829.V1_20_0";
+import { ModalService } from "../../core/modal-service.js?v=20260829.V1_20_0";
+import { DepartmentReadService } from "../../services/department-read-service.js?v=20260829.V1_20_0";
+import { UserReadService } from "../../services/user-read-service.js?v=20260829.V1_20_0";
+import { ExecutiveDirectiveService } from "../../services/executive-directive-service.js?v=20260829.V1_20_0";
 
 let state = {
   directives: [],
