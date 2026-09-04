d457c21c021c93139d57a5ee647c59228de3d5af
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-09-04 13:59:39 +0700

    Add files via upload

--- nhiem-vu/modules/tasks/tasks-view.js
+++ nhiem-vu/modules/tasks/tasks-view.js
@@ -1,11 +1,11 @@
-import { UserContext } from "../../core/user-context.js?v=20260904.V1_22_6";
-import { Permissions } from "../../core/permissions.js?v=20260904.V1_22_6";
-import { ToastService } from "../../core/toast-service.js?v=20260904.V1_22_6";
-import { TaskReadService } from "../../services/task-read-service.js?v=20260904.V1_22_6";
-import { TaskWriteService } from "../../services/task-write-service.js?v=20260904.V1_22_6";
-import { openTaskCreateModal } from "./task-form-modal.js?v=20260904.V1_22_6";
-import { openTaskDetailModal } from "./task-detail-modal.js?v=20260904.V1_22_6";
-import { effectiveDepartmentAssignmentStatus } from "../../core/task-display-order.js?v=20260904.V1_22_6";
+import { UserContext } from "../../core/user-context.js?v=20260903.V1_22_5";
+import { Permissions } from "../../core/permissions.js?v=20260903.V1_22_5";
+import { ToastService } from "../../core/toast-service.js?v=20260903.V1_22_5";
+import { TaskReadService } from "../../services/task-read-service.js?v=20260903.V1_22_5";
+import { TaskWriteService } from "../../services/task-write-service.js?v=20260903.V1_22_5";
+import { openTaskCreateModal } from "./task-form-modal.js?v=20260903.V1_22_5";
+import { openTaskDetailModal } from "./task-detail-modal.js?v=20260903.V1_22_5";
+import { effectiveDepartmentAssignmentStatus } from "../../core/task-display-order.js?v=20260903.V1_22_5";
 
 let renderSequence = 0;
 let currentTasks = [];
