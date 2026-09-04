f74890515f675d24c031ced6c77bad8af0ca87c0
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-09-04 07:48:59 +0700

    Add files via upload

--- nhiem-vu/core/auth-service.js
+++ nhiem-vu/core/auth-service.js
@@ -13,8 +13,8 @@
  * - Giữ nguyên UID Firebase và mô hình accessAccounts hiện hữu.
  */
 
-import { FirebaseService } from "./firebase-service.js?v=20260903.V1_22_3";
-import { UserContext } from "./user-context.js?v=20260903.V1_22_3";
+import { FirebaseService } from "./firebase-service.js?v=20260903.V1_22_5";
+import { UserContext } from "./user-context.js?v=20260903.V1_22_5";
 
 const LOGIN_URL = "./login.html";
 const AUTH_TIMEOUT_MS = 10000;
