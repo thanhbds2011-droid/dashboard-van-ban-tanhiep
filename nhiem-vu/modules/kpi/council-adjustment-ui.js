ff62ccb9cec4829fe1782d0d17227fc73b86d8dd
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-09-04 13:57:32 +0700

    Add files via upload

--- nhiem-vu/modules/kpi/council-adjustment-ui.js
+++ nhiem-vu/modules/kpi/council-adjustment-ui.js
@@ -1,13 +1,13 @@
 /** Giao diện quy trình điều chỉnh sau Hội đồng - V1.10.0. */
-import { UserContext } from "../../core/user-context.js?v=20260904.V1_22_6";
-import { Permissions } from "../../core/permissions.js?v=20260904.V1_22_6";
-import { FirebaseService } from "../../core/firebase-service.js?v=20260904.V1_22_6";
-import { PeriodReadService } from "../../services/period-read-service.js?v=20260904.V1_22_6";
-import { DepartmentReadService } from "../../services/department-read-service.js?v=20260904.V1_22_6";
-import { UserReadService } from "../../services/user-read-service.js?v=20260904.V1_22_6";
-import { DriveEvidenceService } from "../../services/drive-evidence-service.js?v=20260904.V1_22_6";
-import { CouncilAdjustmentService } from "../../services/council-adjustment-service.js?v=20260904.V1_22_6";
-import { ModalService } from "../../core/modal-service.js?v=20260904.V1_22_6";
+import { UserContext } from "../../core/user-context.js?v=20260903.V1_22_5";
+import { Permissions } from "../../core/permissions.js?v=20260903.V1_22_5";
+import { FirebaseService } from "../../core/firebase-service.js?v=20260903.V1_22_5";
+import { PeriodReadService } from "../../services/period-read-service.js?v=20260903.V1_22_5";
+import { DepartmentReadService } from "../../services/department-read-service.js?v=20260903.V1_22_5";
+import { UserReadService } from "../../services/user-read-service.js?v=20260903.V1_22_5";
+import { DriveEvidenceService } from "../../services/drive-evidence-service.js?v=20260903.V1_22_5";
+import { CouncilAdjustmentService } from "../../services/council-adjustment-service.js?v=20260903.V1_22_5";
+import { ModalService } from "../../core/modal-service.js?v=20260903.V1_22_5";
 
 const PROFESSIONAL_DEPARTMENTS = Object.freeze(["TCHC", "CTXH", "KHTC", "YT", "KI", "KII", "KIII"]);
 
