91c9107bcce91101cf12bbdca786749e3d04fde8
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-09-16 15:14:09 +0700

    Add files via upload

--- nhiem-vu/modules/evaluations/evaluations-view.js
+++ nhiem-vu/modules/evaluations/evaluations-view.js
@@ -1,4 +1,4 @@
-import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260916.V1_24_6";
+import { renderKpiWorkflow } from "../kpi/kpi-workflow.js?v=20260916.V1_24_7";
 export async function renderEvaluationsView(outlet) {
   await renderKpiWorkflow(outlet, { mode: "evaluations" });
 }
