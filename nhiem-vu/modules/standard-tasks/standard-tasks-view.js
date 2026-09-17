ab140a4fa9432876e6836ae3195560a5bd72acfc
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-09-17 11:16:37 +0700

    Add files via upload

--- nhiem-vu/modules/standard-tasks/standard-tasks-view.js
+++ nhiem-vu/modules/standard-tasks/standard-tasks-view.js
@@ -1,12 +1,12 @@
-import { UserContext } from "../../core/user-context.js?v=20260916.V1_24_7";
-import { Permissions } from "../../core/permissions.js?v=20260916.V1_24_7";
-import { ToastService } from "../../core/toast-service.js?v=20260916.V1_24_7";
-import { ModalService } from "../../core/modal-service.js?v=20260916.V1_24_7";
-import { StandardTaskReadService } from "../../services/standard-task-read-service.js?v=20260916.V1_24_7";
-import { PeriodReadService } from "../../services/period-read-service.js?v=20260916.V1_24_7";
-import { StandardTaskWriteService } from "../../services/standard-task-write-service.js?v=20260916.V1_24_7";
-import { TaskRegistrationService } from "../../services/task-registration-service.js?v=20260916.V1_24_7";
-import { deriveDeadlinePlan, deadlineRuleDescription, requiresManualDeadline, isEventDrivenFrequency, canonicalFrequency, STANDARD_FREQUENCIES, WEEKDAY_OPTIONS } from "../../core/deadline-engine.js?v=20260916.V1_24_7";
+import { UserContext } from "../../core/user-context.js?v=20260917.V1_24_8";
+import { Permissions } from "../../core/permissions.js?v=20260917.V1_24_8";
+import { ToastService } from "../../core/toast-service.js?v=20260917.V1_24_8";
+import { ModalService } from "../../core/modal-service.js?v=20260917.V1_24_8";
+import { StandardTaskReadService } from "../../services/standard-task-read-service.js?v=20260917.V1_24_8";
+import { PeriodReadService } from "../../services/period-read-service.js?v=20260917.V1_24_8";
+import { StandardTaskWriteService } from "../../services/standard-task-write-service.js?v=20260917.V1_24_8";
+import { TaskRegistrationService } from "../../services/task-registration-service.js?v=20260917.V1_24_8";
+import { deriveDeadlinePlan, deadlineRuleDescription, requiresManualDeadline, isEventDrivenFrequency, canonicalFrequency, STANDARD_FREQUENCIES, WEEKDAY_OPTIONS } from "../../core/deadline-engine.js?v=20260917.V1_24_8";
 
 let currentCatalogAccess = {
   canManage: false,
